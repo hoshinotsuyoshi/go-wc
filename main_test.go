f3c877a650ce47083acaefbf3ccf45a91e7f2880
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2017-12-10 23:18:50 +0900

    Add test for bytes

--- main_test.go
+++ main_test.go
@@ -15,13 +15,24 @@ func ExampleCounter() {
 	// true
 }
 
-func ExampleCounter_Show() {
+func ExampleCounter_Show_lines() {
 	c := main.Counter{}
 	opts := main.FlagOptions{}
 	opts.PrintLines = true
-	reader := strings.NewReader("abc\ndef\n")
+	reader := strings.NewReader("あいう\ndef\n")
 	c.Count(reader)
 	c.Show(&opts, "filename")
 	// Output:
 	// 2 filename
+}
+
+func ExampleCounter_Show_bytes() {
+	c := main.Counter{}
+	opts := main.FlagOptions{}
+	opts.PrintBytes = true
+	reader := strings.NewReader("あいう\ndef\n")
+	c.Count(reader)
+	c.Show(&opts, "filename")
+	// Output:
+	// 14 filename
 }
